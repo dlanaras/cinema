b79323b2ea9518e63c60848dc490d72dbc7960fa
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-15 13:29:13 +0100

    created basic class with empty method

--- room.class.php
+++ room.class.php
@@ -1,2 +1,13 @@
 <?php
 
+class Room 
+{
+    public $roomName;
+    public $totalSeats;
+
+    public function __construct($roomName, $totalSeats) 
+    {
+        $this->roomName = $roomName;
+        $this->totalSeats= $totalSeats;
+    }
+}
