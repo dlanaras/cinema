80d1f0edd88ef78e7324ad07e4860847076734f6
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-15 13:25:05 +0100

    basic class with empty methods

--- customer.class.php
+++ customer.class.php
@@ -1,2 +1,26 @@
 <?php 
 
+class Customer 
+{
+    public $prename;
+    public $lastname;
+    public $age;
+    public $date;
+    public $film;
+
+    public function __construct($prename, $lastname, $age, $date, $film) {
+        $this->prename = $prename;
+        $this->lastname = $lastname;
+        $this->age = $age;
+        $this->date = $date;
+        $this->film = $film;
+    }
+
+    public function register_customer() {
+
+    }
+
+    public function create_booking() {
+        
+    }
+}
