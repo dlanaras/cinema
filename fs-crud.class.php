0a3636362ab416ac5ec991e6b5969b14743a229c
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-15 13:36:42 +0100

    added basic class with 4 empty methods

--- fs-crud.class.php
+++ fs-crud.class.php
@@ -1,2 +1,42 @@
 <?php
 
+class FS_CRUD 
+{
+    public $filmName;
+    public $filmDesc;
+    public $filmDatetime;
+    public $cinemaRoom;
+    public $price;
+    public $ageRestriction;
+
+    public function __construct($filmName, $filmDesc, $filmDatetime, $cinemaRoom, $price, $ageRestriction) 
+    {
+        $this->filmName = $filmName;
+        $this->filmDesc = $filmDesc;
+        $this->filmDatetime = $filmDatetime;
+        $this->cinemaRoom = $cinemaRoom;
+        $this->price = $price;
+        $this->ageRestriction = $ageRestriction;
+    }
+
+
+    public function create_FS() 
+    {
+
+    }
+
+    public function add_To_FS()
+    {
+
+    }
+
+    public function remove_From_FS()
+    {
+
+    }
+
+    public function replace_From_FS() 
+    {
+        
+    }
+}
