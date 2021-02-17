16aaa3adb6adf701f4993dbd5a35ae27c1ac8575
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-17 13:58:55 +0100

    altered and finished second method

--- fs-crud.class.php
+++ fs-crud.class.php
@@ -24,6 +24,7 @@ class FS_CRUD //Film Schedule - Create Remove Update Delete
     {
         //Choose Day for Film-Schedule (Name of Film-Schedule includes day)
         $answer = "ja";
+        echo "Da können Sie weitere Filme in einen schon existiereneden Film-Plan einfügen oder einen neuen Filmplan erstellen\n";
         $FSDay = readline("An welchem Tag wird dieser Film-Plan stattfinden (Format: DD_MM_YYYY): \n");
         $filePath = "./data/FS_" . $FSDay .".json";
         $handle = fopen($filePath, "a+");
@@ -62,28 +63,69 @@ class FS_CRUD //Film Schedule - Create Remove Update Delete
 
                 $jsonData = json_encode($filler);
                 fwrite($handle, "[" . $jsonData . "]"); //added Square brackets to match the other json objects/arrays
-                
+
                 //file_put_contents($filePath, $filler); Didn't want to work had to use fwrite() instead
                 fclose($handle);
             }
         }
     }
 
-    public function add_To_FS()
-    {
+    // was originally a method for appending, but since the othere method does that i decided to replace it with a method that shows a specific Film-Schedule
 
+    public function show_FS()
+    {
+        //recomended to open cmd twice one to be able to see the list here and the other to select what to delete and what to replace
+
+        $directory = "./data/";
+        $texts = glob($directory . "FS_*.json");
+        echo "Die folgende Dateien sind die schon existierende Film-Pläne.\n";
+
+
+        foreach($texts as $text)
+        {
+            echo $text . "\n";
+        }
+
+    $FSDay = readline("\nUm einen Film-Plan genauer anzuschauen geben Sie die Datum des Film-Plans ein (FS_ '01_01_2000' .json): \n");
+    $filePath = "./data/FS_" . $FSDay .".json";
+    $FSData = file_get_contents($filePath);
+    $FSDecode = json_decode($FSData);
+
+    foreach($FSDecode as $results) 
+    {
+        //print_r($testing = array_slice($results, 1, sizeof($results, 0)));
+        foreach($results as $result)
+        {
+            echo "----------------------------------------------------------\n";
+            echo "Name: " . $result->{'filmName'} . "\n";
+            echo "Beschreibung: " . $result->{'filmDesc'} . "\n";
+            echo "Zeit: " . $result->{'filmDatetime'} . "\n";
+            echo "Halle: " . $result->{'cinemaRoom'} . "\n";
+            echo "Preis: " . $result->{'price'} . "\n";
+            echo "Altersfreigabe: " . $result->{'ageRestriction'} . "\n";
+            echo "----------------------------------------------------------\n\n";
+        }
     }
+    }
+
 
     public function remove_From_FS()
     {
-
+        //TODO: use name, room and datetime for the deletion of a whole array
+        //This should also be used to remove the fillers
     }
 
     public function replace_From_FS() 
     {
-        
+        //TODO: show all information that can be each selected for replace
     }
 }
 
-$fscreate = new FS_CRUD("", "", "", "", "", "");
-$fscreate->create_FS();
+// Create a new Film-Schedule for a specific day (NEW: also used to append) TODO: remember that first object should be ignored
+/*
+$FScreate = new FS_CRUD("", "", "", "", "", "");
+$FScreate->create_FS();
+*/
+
+$FSshow = new FS_CRUD("","","","","","");
+$FSshow->show_FS();
