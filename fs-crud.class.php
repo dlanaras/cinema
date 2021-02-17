aab14304ecad1b409c9b2be1aff33b58bc8fa376
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-17 11:00:25 +0100

    created the first Method of FS-CRUD (The C part)

--- fs-crud.class.php
+++ fs-crud.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class FS_CRUD 
+class FS_CRUD //Film Schedule - Create Remove Update Delete
 {
     public $filmName;
     public $filmDesc;
@@ -22,7 +22,51 @@ class FS_CRUD
 
     public function create_FS() 
     {
+        //Choose Day for Film-Schedule (Name of Film-Schedule includes day)
+        $answer = "ja";
+        $FSDay = readline("An welchem Tag wird dieser Film-Plan stattfinden (Format: DD_MM_YYYY): \n");
+        $filePath = "./data/FS_" . $FSDay .".json";
+        $handle = fopen($filePath, "a+");
 
+
+        while($answer != "nein")
+        {
+            $oldData = file_get_contents($filePath);
+            $tempArray = json_decode($oldData);
+
+            if ($oldData != NULL) 
+            {
+                $filmName = readline("Wie heisst der Film: \n");
+                $filmDesc = readline("Geben Sie eine kurze Beschreibung des Filmes: \n");
+                $filmDatetime = readline("Wann findet der Film statt (Format: DD.MM.YYYY:HH:MM:SS): \n");
+                $cinemaRoom = readline("In welcher Halle findet der Film statt: \n");
+                $price = readline("Wie viel Kostet ein Billet für diesen Film (Format: 12.35): \n");
+                $ageRestriction = readline("Was ist die Altersfreigabe für diesen Film: \n");
+
+                $array = 
+                [
+                $test = new FS_CRUD($filmName, $filmDesc, $filmDatetime, $cinemaRoom, $price, $ageRestriction)
+                ];
+
+                array_push($tempArray, $array);
+                $jsonData = json_encode($tempArray);
+                file_put_contents($filePath, $jsonData);
+                $answer = readline("Wollen Sie einen weiteren Film einfügen (Die Enter-Taste drücken um weitere Filme einfügen, sonst 'nein' eingeben): \n");
+            } 
+            else 
+            {
+                $filler =
+                array(
+                $fill = new FS_CRUD("first", "JSON", "array", "is", "a", "filler")
+                );
+
+                $jsonData = json_encode($filler);
+                fwrite($handle, "[" . $jsonData . "]"); //added Square brackets to match the other json objects/arrays
+                
+                //file_put_contents($filePath, $filler); Didn't want to work had to use fwrite() instead
+                fclose($handle);
+            }
+        }
     }
 
     public function add_To_FS()
@@ -40,3 +84,6 @@ class FS_CRUD
         
     }
 }
+
+$fscreate = new FS_CRUD("", "", "", "", "", "");
+$fscreate->create_FS();
