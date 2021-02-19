308283b2e6fc1af1482b18114b3a1d1e49fba0e0
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-02-19 11:53:51 +0100

    rooms.class assigned assigned all rooms with seats

--- room.class.php
+++ room.class.php
@@ -11,9 +11,45 @@ class Room
         $this->seats = $seats;
     }
 
-    public function assign_seats($roomName, $seats)
+    private function first_assign()
     {
-        fopen("./data/rooms.json");
+        $handle = fopen("./data/rooms.json", "a+");
         
+        for($i = 1; $i < 10; $i++)
+        {
+            $oldData = file_get_contents("./data/rooms.json");
+            $tempArray = json_decode($oldData);
+
+            if ($oldData != NULL) 
+            {
+                $seats = readline("Wie viele Plätze gibt es in der Halle " . $i . "? \n"); 
+                $array = 
+                [
+                $test = new Room($i, $seats)
+                ];
+
+                array_push($tempArray, $array);
+                $jsonData = json_encode($tempArray);
+                file_put_contents("./data/rooms.json", $jsonData);
+            } 
+            else 
+            {
+                $seats = readline("Wie viele Plätze gibt es in der Halle 1? \n"); 
+                $first =
+                array(
+                $theFirst = new Room($i, $seats)
+                );
+
+                $jsonData = json_encode($first);
+                fwrite($handle, "[" . $jsonData . "]"); //added Square brackets to match the other json objects/arrays
+                fclose($handle);
+            }
+        }
     }
+    //let user assign seats
 }
+
+/* should only be used once
+$firstAssign = new Room("", "");
+$firstAssign->first_assign();
+*/
